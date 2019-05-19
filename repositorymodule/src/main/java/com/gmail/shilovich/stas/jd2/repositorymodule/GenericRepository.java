0f6590003a96abe9605f82e0c0bf2ab802263cde
Stas <stas.shilovich@gmail.com>
2019-05-19 22:57:38 +0300

    Add hibernate basic

--- repositorymodule/src/main/java/com/gmail/shilovich/stas/jd2/repositorymodule/GenericRepository.java
+++ repositorymodule/src/main/java/com/gmail/shilovich/stas/jd2/repositorymodule/GenericRepository.java
@@ -1,8 +1,23 @@
 package com.gmail.shilovich.stas.jd2.repositorymodule;
 
 import java.sql.Connection;
+import java.util.List;
 
-public interface GenericRepository {
+public interface GenericRepository<I, T> {
 
     Connection getConnection();
+
+    void persist(T entity);
+
+    void merge(T entity);
+
+    void remove(T entity);
+
+    T findById(I id);
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    List<T> findAll(int offset, int limit);
+
+    int getCountOfEntities();
+
 }
